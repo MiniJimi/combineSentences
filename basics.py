cd0cf640babeef76d6f02c63558e5b7407b428c6
MiniJimi <47718656+MiniJimi@users.noreply.github.com>
2021-11-06 22:37:59 -0400

    init

--- basics.py
+++
@@ -1,9 +0,0 @@
-#student_grades={9.1, 8.8, 7.5}
-
-#the fact that python doesn't have a min function, we create one
- 
-def mean(mylist):       # define        nameFunction(parameters):
-    the_mean=sum((mylist)/len(mylist))
-    return the_mean
-
-print(mean([1,4,6]))
--- basics_old.py
+++ basics_old.py
@@ -1,1 +1,4 @@
-print(24*7)
+def celsius_to_kelvin(cels):
+    cels=input("Enter celsius: ")
+    conv=float(cels)+273.15
+    return "CONVERSION %s" %conv
