14eee6198d596f8c2e534dd2b210f04bf516aab6
MiniJimi <47718656+MiniJimi@users.noreply.github.com>
2021-11-30 19:27:11 -0500

    update

--- moreBasics.py
+++ moreBasics.py
@@ -1,4 +1,23 @@
-with open("hiddenGem/file.txt", "w") as file:
-    content=file.write("snail")
-    
+import time
+import os
+
+#while True:
+#    if os.path.exists("hiddenGem/data"):
+ #       with open("hiddenGem/data") as file:
+ #           print(file.read())
+ #   else:
+ #       print("File does not exist.")
+ #   time.sleep(5)
+
+while True:
+    if os.path.exists("hiddenGem/data"):
+        with open("hiddenGem/data") as file:
+            print(file.read())
+    else:
+        print("File does not exist")
+        
+    time.sleep(3)
+
+
+        
 
